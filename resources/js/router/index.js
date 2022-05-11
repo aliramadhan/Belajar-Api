cf55ce2b5daec91c4110992c2785763b83a9ceb4
aliramadhan <yuukikimisaki@gmail.com>
2022-05-11 15:00:02 +0700

    add file laravel

---
+++ resources/js/router/index.js
@@ -0,0 +1,61 @@
+import {createWebHistory, createRouter} from "vue-router";
+
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Register from '../pages/Register';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+
+import Books from '../components/Books/Index';
+import AddBook from '../components/Books/Create';
+import EditBook from '../components/Books/Edit';
+
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Home
+    },
+    {
+        name: 'about',
+        path: '/about',
+        component: About
+    },
+    {
+        name: 'register',
+        path: '/register',
+        component: Register
+    },
+    {
+        name: 'login',
+        path: '/login',
+        component: Login
+    },
+    {
+        name: 'dashboard',
+        path: '/dashboard',
+        component: Dashboard
+    },
+    {
+        name: 'books',
+        path: '/books',
+        component: Books
+    },
+    {
+        name: 'addbook',
+        path: '/books/add',
+        component: AddBook
+    },
+    {
+        name: 'editbook',
+        path: '/books/edit/:id',
+        component: EditBook
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+});
+
+export default router;
